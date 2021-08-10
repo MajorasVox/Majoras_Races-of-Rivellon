02d799f7ad76c93da6dd0c36dadcbdb3c2d6a986
Majoras Vox <45341180+MajorasVox@users.noreply.github.com>
2021-08-10 21:09:28 +0100

    Nift LL Script
    Nifty script LL fashioned that I use to make sure the Race Skill/Abillites replace Dome of Protection & Flame Breath

--- Mods/MAJORAS_ROR_DE_ccd3b98e-d611-4612-8e59-1747bb1d1923/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/MAJORAS_ROR_DE_ccd3b98e-d611-4612-8e59-1747bb1d1923/Story/RawFiles/Lua/BootstrapServer.lua
@@ -1,2 +1,14 @@
 Ext.Require("Shared.lua")
 Ext.Require("Server/Armor/_InitArmor.lua")
+
+RegisterListener("Initialized", function()
+    if CharacterHasSkill(Origins.DemonicKin, "Cone_Flamebreath") == 1 then
+      GameHelpers.Skill.Swap(Origins.DemonicKin, "Cone_Flamebreath", "Target_ConsumeEnemy", true, false)
+    end
+  end)
+
+RegisterListener("Initialized", function()
+  if CharacterHasSkill(Origins.DemonicKin, "Dome_CircleOfProtection") == 1 then
+    GameHelpers.Skill.Swap(Origins.DemonicKin, "Dome_CircleOfProtection", "Shout_Demon_Wings", true, false)
+  end
+end)
