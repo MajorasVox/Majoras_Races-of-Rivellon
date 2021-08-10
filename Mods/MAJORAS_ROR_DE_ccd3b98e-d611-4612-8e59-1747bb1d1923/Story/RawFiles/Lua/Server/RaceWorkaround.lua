912dab3dd76f58ff205c2f545164105e27d20836
LaughingLeader <laughingleader@gmail.com>
2021-08-10 16:03:54 -0500

    Added a script for overriding race queries or custom origins

---
+++ Mods/MAJORAS_ROR_DE_ccd3b98e-d611-4612-8e59-1747bb1d1923/Story/RawFiles/Lua/Server/RaceWorkaround.lua
@@ -0,0 +1,55 @@
+--[[
+Base Races:
+Dwarf
+Elf
+Human
+Lizard
+Undead_Dwarf
+Undead_Elf
+Undead_Human
+Undead_Lizard
+]]
+
+local undeadRace = {
+	Undead_Dwarf = true,
+	Undead_Elf = true,
+	Undead_Human = true,
+	Undead_Lizard = true,
+}
+
+local OriginRaces = {
+	[Origins.AncientElf] = "Elf",
+	[Origins.DemonicKin] = "Undead_Lizard",
+	[Origins.FailedGheist] = "Human",
+	[Origins.LivingBear] = "Undead_Dwarf",
+	[Origins.Zombie] = "Undead_Human",
+}
+
+--Making sure QRY_IsTrueRace doesn't return true for whatever race our origins might be inherently
+Ext.RegisterOsirisListener("QRY_IsTrueRace", 2, "after", function(uuid, race)
+	uuid = StringHelpers.GetUUID(uuid)
+	local trueRace = OriginRaces[uuid]
+	if trueRace ~= nil then
+		return trueRace == race
+	end
+end)
+
+--Overriding the QRY_IsTrueUndead result so it works regardless of the CharacterGetRace result.
+Ext.RegisterOsirisListener("QRY_IsTrueUndead", 1, "after", function(uuid)
+	uuid = StringHelpers.GetUUID(uuid)
+	local trueRace = OriginRaces[uuid]
+	if trueRace ~= nil then
+		return undeadRace[trueRace] == true
+	end
+end)
+
+Ext.RegisterOsirisListener("CharacterGetRace", 3, "before", function(uuid, canPolymorphOverride, raceResult)
+	if canPolymorphOverride ~= 1 then
+		--Strip away the Name_ part
+		uuid = StringHelpers.GetUUID(uuid)
+		local trueRace = OriginRaces[uuid]
+		if trueRace ~= nil then
+			return trueRace
+		end
+	end
+end)
