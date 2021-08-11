1485d922d3c99abdbeea9f125d8d54119fa4d0a6
Majoras Vox <45341180+MajorasVox@users.noreply.github.com>
2021-08-11 19:19:40 +0100

    Update BootstrapServer.lua
    Updated names of the Demons racial ability & place holder skill.

--- Mods/MAJORAS_ROR_DE_ccd3b98e-d611-4612-8e59-1747bb1d1923/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/MAJORAS_ROR_DE_ccd3b98e-d611-4612-8e59-1747bb1d1923/Story/RawFiles/Lua/BootstrapServer.lua
@@ -2,13 +2,11 @@ Ext.Require("Shared.lua")
 Ext.Require("Server/Armor/_InitArmor.lua")
 
 RegisterListener("Initialized", function()
-    if CharacterHasSkill(Origins.DemonicKin, "Cone_Flamebreath") == 1 then
-      GameHelpers.Skill.Swap(Origins.DemonicKin, "Cone_Flamebreath", "Target_ConsumeEnemy", true, false)
-    end
-  end)
-
-RegisterListener("Initialized", function()
+  if CharacterHasSkill(Origins.DemonicKin, "Cone_Flamebreath") == 1 then
+      GameHelpers.Skill.Swap(Origins.DemonicKin, "Cone_Flamebreath", "Target_ROR_DK_AllConsumingHunger", true, false)
+  end
+  
   if CharacterHasSkill(Origins.DemonicKin, "Dome_CircleOfProtection") == 1 then
-    GameHelpers.Skill.Swap(Origins.DemonicKin, "Dome_CircleOfProtection", "Shout_Demon_Wings", true, false)
+      GameHelpers.Skill.Swap(Origins.DemonicKin, "Dome_CircleOfProtection", "Shout_ROR_PlaceHolder", true, false)
   end
 end)
