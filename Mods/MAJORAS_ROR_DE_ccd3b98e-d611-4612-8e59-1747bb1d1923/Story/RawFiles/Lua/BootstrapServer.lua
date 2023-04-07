f983a045dc420ef2bba5fd287a098b193e40aedd
LaughingLeader <laughingleader@gmail.com>
2023-04-07 08:40:01 -0500

    Remove _InitArmor require line

--- Mods/MAJORAS_ROR_DE_ccd3b98e-d611-4612-8e59-1747bb1d1923/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/MAJORAS_ROR_DE_ccd3b98e-d611-4612-8e59-1747bb1d1923/Story/RawFiles/Lua/BootstrapServer.lua
@@ -1,12 +1,9 @@
 Ext.Require("Shared.lua")
 
-VM = VisualManager.Elements
-
-Ext.Require("Server/Armor/_InitArmor.lua")
 Ext.Require("Server/OriginSetup.lua")
 Ext.Require("Server/RaceWorkaround.lua")
 Ext.Require("Server/SkillListeners.lua")
 
-if Ext.IsDeveloperMode() then
+if Ext.Debug.IsDeveloperMode() then
     Ext.Require("Server/DebugCommands.lua")
 end
--- Mods/MAJORAS_ROR_DE_ccd3b98e-d611-4612-8e59-1747bb1d1923/Story/RawFiles/Lua/Server/DebugCommands.lua
+++ Mods/MAJORAS_ROR_DE_ccd3b98e-d611-4612-8e59-1747bb1d1923/Story/RawFiles/Lua/Server/DebugCommands.lua
@@ -1,3 +1,5 @@
+local VM = VisualManager.Elements
+
 Ext.RegisterConsoleCommand("ror_fj_statue", function(cmd)
 	Osi.TeleportTo(CharacterGetHostCharacter(), "cdfacbb7-acf8-4c3e-a5a2-bac8a5b0e5a0")
 end)
